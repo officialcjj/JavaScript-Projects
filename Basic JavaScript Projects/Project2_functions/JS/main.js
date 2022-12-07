f0bea5cd88cdc3be7db186cb960a487dc171c7f1
officialcjj <officialcjj@gmail.com>
2022-12-07 15:58:29 -0800

    Basic Buttons

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,15 @@
+var A="Sample_1"
+
+var B="Sample_2"
+
+function Sum() {
+    document.getElementById("button") 
+    .innerHTML = A+B;
+}
+
+function Concatenate() {
+    var sentence = "I'm learning"
+    sentence += " a lot"
+    document.getElementById("concatenate")
+    .innerHTML = sentence;
+}
