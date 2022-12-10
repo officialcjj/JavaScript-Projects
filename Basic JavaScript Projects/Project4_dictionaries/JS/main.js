1daac1d5abf7e4dd93f82dea460a7bec4d13d99e
officialcjj <officialcjj@gmail.com>
2022-12-09 18:25:39 -0800

    Value Deletion Example

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,11 @@
+function myDictionary() {
+    var Animal = {
+        Species:"Dog",
+        Color:"Black",
+        Breed:"Labrador",
+        Age:5,
+        Sound:"Bark!"
+    };
+    delete Animal.Sound;
+    document.getElementById("Dictionary").innerHTML = Animal.Sound;
+}
